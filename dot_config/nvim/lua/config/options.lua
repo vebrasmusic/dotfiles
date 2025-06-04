4218aac23fff91b1f2ca81f752d3e0bace280700
Andrés Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2025-06-04 15:53:17 -0700

    added snippets! ctrl j

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,6 +3,8 @@
 -- Add any additional options here
 vim.g.root_spec = { "cwd" }
 
+vim.g.lazyvim_mini_snippets_in_completion = false
+
 vim.g.dbs = {
   dev = "postgresql://usr:pass@localhost:5432/postgres",
 }
