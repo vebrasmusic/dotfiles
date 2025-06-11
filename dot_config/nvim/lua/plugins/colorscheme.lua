3e32c3a4ed55793341755944bfd09bae92e61ead
Andrés Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2025-06-10 21:09:41 -0700

    add poimandres

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -12,12 +12,58 @@ return {
     end,
   }, -- Configure LazyVim to load gruvbox
   {
+    "xiyaowong/transparent.nvim",
+    config = function()
+      require("transparent").setup({
+        extra_groups = {
+          "Normal",
+          "NormalNC",
+          "NormalFloat",
+          "NvimTreeNormal",
+          "TelescopeNormal",
+          "TelescopeBorder",
+          "FloatBorder",
+          "StatusLine",
+          "EndOfBuffer",
+        },
+        exclude_groups = {},
+      })
+      vim.cmd("TransparentEnable")
+    end,
+  },
+  {
     "rebelot/kanagawa.nvim",
+  },
+  {
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      local p = require("poimandres.palette")
+      require("poimandres").setup({
+        bold_vert_split = false, -- use bold vertical separators
+        disable_float_background = false, -- disable background for floats
+        highlight_groups = {
+          LspReferenceText = { bg = p.background1 },
+          LspReferenceRead = { bg = p.background1 },
+          LspReferenceWrite = { bg = p.background1 },
+        },
+      })
+    end,
+  },
+  {
+    "2giosangmitom/nightfall.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {}, -- Add custom configuration here
+    config = function(_, opts)
+      require("nightfall").setup(opts)
+    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa-dragon",
+      colorscheme = "poimandres",
     },
   },
 }
