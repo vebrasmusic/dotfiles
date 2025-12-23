d1a202708a85cfd961ba8551008960e92043e063
Andrés Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2025-12-23 13:50:52 -0800

    get rid of some stuff, do some other stuff

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,3 @@
--- undo tree
-vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
-
 -- global note
 local global_note = require("global-note")
 vim.keymap.set("n", "<leader>gn", global_note.toggle_note, {
